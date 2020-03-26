74777b434980f3d5542556db2bab1c7c29b3a141
Vitaliy Puhliy <vitaliydens@gmail.com>
2020-03-26 15:30:54 +0200

    Init framework

---
+++ LococoFrameWork/LococoFrameWork/LococoFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  LococoFrameWork.h
+//  LococoFrameWork
+//
+//  Created by Developer on 26.03.2020.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LococoFrameWork.
+FOUNDATION_EXPORT double LococoFrameWorkVersionNumber;
+
+//! Project version string for LococoFrameWork.
+FOUNDATION_EXPORT const unsigned char LococoFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LococoFrameWork/PublicHeader.h>
+
+
---
+++ LococoFrameWork/LococoFrameWork/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  LococoFrameWork
+//
+//  Created by Developer on 26.03.2020.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    public func dothing() {
+        print("mmmmmmmmm")
+    }
+}
